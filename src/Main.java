1366d535d9b57301d8e4b2e997cb07c6d017f2c1
SalvatoreCirasa <uni389305@studium.unict.it>
2023-09-14 16:07:39 +0200

    esercizio1.

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
         conta1.setConta(5);
         System.out.println(conta1.getConta());
 
-        //-------------------------------
+        //---------------------------
         System.out.println(" ");
 
         System.out.println(conta2.getConta());
